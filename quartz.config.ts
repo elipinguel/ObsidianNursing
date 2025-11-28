d7a78f28695f7b53e08a5669e246da018ab6cefe
elipinguel <elipinguel@gmail.com>
2025-11-27 23:41:47 -0600

    Quartz sync: Nov 27, 2025, 11:41 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "elipinguel.github.io/NursingVault",
+    baseUrl: "elipinguel.github.io/ObsidianNursing",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
